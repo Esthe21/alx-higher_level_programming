5f761ff63a6ed038b6db2b473c0391621e1d309d
Esther Dogbey <sthrdogbey@gmail.com>
2023-09-11 19:45:52 +0000

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# element_at = __import__('1-element_at').element_at
+
+def replace_in_list(my_list, idx, element):
+	"""Replace an element of a list at a particular position."""
+	if idx >= 0 and idx < len(my_list):
+	my_list[idx] = element
+	return (my_list)
+
